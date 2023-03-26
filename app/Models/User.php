26b53eceaeedbe0607ba5dd26d343a88bbe85225
Odame <kwesi.danquah@daaseng.com>
2023-03-26 19:45:31 +0100

    specify relation between role and user

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,4 +39,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->hasOne(Role::class, 'id', 'role_id');
+        // return $this->belongsTo(Role::class, 'role_id');
+    }
 }
